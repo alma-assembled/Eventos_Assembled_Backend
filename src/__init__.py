f529a710948f580601252e973a9e47ce113d196a
alma-assembled <desarrollo@assembled.mx>
2024-07-09 09:55:25 -0600

    docker

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from .routes import IndexRoutes, EventosRoutes
 
 app = Flask(__name__) 
-CORS(app, resources={r"/eventos/*": {"origins": "http://localhost:3000"}})
+CORS(app)
 
 def init_app(config):
     # Configuration
@@ -13,5 +13,6 @@ def init_app(config):
     # Blueprints
     app.register_blueprint(IndexRoutes.main, url_prefix='/')
     app.register_blueprint(EventosRoutes.main, url_prefix='/eventos')
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
     return app
